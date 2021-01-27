5b1c281f577dbc0b95529cf1a0b090ba52666eed
raj3priya <b.priyarenu@gmail.com>
2021-01-27 22:06:55 +0530

    added education entity

---
+++ src/app/model/EducationDetails.ts
@@ -0,0 +1,12 @@
+export class EducationDetails {
+    type:string;
+    degree:string;
+    branch:string;
+    mode:string;
+    startDate:Date;
+    endDate:Date;
+    institution:string;
+    state:string;
+    city:string;
+    percentage:number;
+}
