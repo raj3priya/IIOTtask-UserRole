c8a15f5588fbdd1607f7bbe9d34664a9ade91359
SowmyaS98 <sowmyselvaraj.ssk@gmail.com>
2021-01-28 15:13:30 +0530

    bootstarp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DocumentsUploadComponent } from './documents-upload/documents-upload.component';
 import { EducationComponent } from './education/education.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: 'personal-details', component: RegisterComponent},
   { path: 'education-details', component: EducationComponent},
+  {path: 'documents-upload',component: DocumentsUploadComponent}
 ];
 
 @NgModule({
