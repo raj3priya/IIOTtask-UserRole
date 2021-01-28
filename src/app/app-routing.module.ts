a6e1a0237432e30d3400d2c1d48b84cacd2c1dee
raj3priya <b.priyarenu@gmail.com>
2021-01-28 15:44:16 +0530

    added path for work experience

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EducationComponent } from './education/education.component';
 import { RegisterComponent } from './register/register.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
 const routes: Routes = [
   {path: 'personal-details', component: RegisterComponent},
   { path: 'education-details', component: EducationComponent},
+  {path: 'work-experience', component: WorkExperienceComponent}
 ];
 
 @NgModule({
